17fafdb25fe183f8e92e353fc5806f7cc04d322b
Ali Akbar Mohammadi <kocholsoft@yahoo.com>
2012-06-07 19:03:50 +0330

    Deleting all files.

--- engine/KGEAndroid/jni/Application.mk
+++
@@ -1,9 +0,0 @@
-# This project file has been automatically generated.
-# Manual edition is not recommended.
-
-APP_MODULES := \
-    kge
-
-APP_STL := stlport_shared
-APP_OPTIM := release
-APP_ABI := armeabi armeabi-v7a
