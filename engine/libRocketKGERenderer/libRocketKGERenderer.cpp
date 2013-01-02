babab9bfc0dc4a875feaef3b46d07b982af06f51
Ali Akbar Mohammadi <kocholsoft@yahoo.com>
2013-01-03 02:54:05 +0330

    Working on libRocketKGERenderer. It dose not render anything yet I don't know why.

--- engine/libRocketKGERenderer/libRocketKGERenderer.cpp
+++ engine/libRocketKGERenderer/libRocketKGERenderer.cpp
@@ -4,6 +4,9 @@
 #include "../include/Texture.h"
 #include "../include/VertexElement.h"
 #include "../include/Matrix.h"
+#include "../include/Image.h"
+#include "../include/Timer.h"
+#include "../include/Logger.h"
 
 //! Renderer public pointer
 KGE_IMPORT extern kge::gfx::Renderer*	g_pRenderer;
@@ -84,7 +87,9 @@ namespace kge
 	//------------------------------------------------------------------------------------
 	// Called by Rocket when it wants to render geometry that it does not wish to optimise.
 	//------------------------------------------------------------------------------------
-	void libRocketKGERenderer::RenderGeometry( Rocket::Core::Vertex* vertices, int num_vertices, int* indices, int num_indices, Rocket::Core::TextureHandle texture, const Rocket::Core::Vector2f& translation )
+	void libRocketKGERenderer::RenderGeometry( Rocket::Core::Vertex* vertices, int num_vertices, 
+		int* indices, int num_indices, Rocket::Core::TextureHandle texture, 
+		const Rocket::Core::Vector2f& translation )
 	{
 
 	} // RenderGeometry
@@ -92,7 +97,9 @@ namespace kge
 	//------------------------------------------------------------------------------------
 	// Called by Rocket when it wants to compile geometry it believes will be static for the forseeable future.
 	//------------------------------------------------------------------------------------
-	Rocket::Core::CompiledGeometryHandle libRocketKGERenderer::CompileGeometry( Rocket::Core::Vertex* vertices, int num_vertices, int* indices, int num_indices, Rocket::Core::TextureHandle texture )
+	Rocket::Core::CompiledGeometryHandle libRocketKGERenderer::CompileGeometry
+		( Rocket::Core::Vertex* vertices, int num_vertices, int* indices, int num_indices, 
+		Rocket::Core::TextureHandle texture )
 	{
 		RocketKGECompiledGeometry* p = KGE_NEW(RocketKGECompiledGeometry);
 
@@ -111,7 +118,8 @@ namespace kge
 	//------------------------------------------------------------------------------------
 	// Called by Rocket when it wants to render application-compiled geometry.
 	//------------------------------------------------------------------------------------
-	void libRocketKGERenderer::RenderCompiledGeometry( Rocket::Core::CompiledGeometryHandle geometry, const Rocket::Core::Vector2f& translation )
+	void libRocketKGERenderer::RenderCompiledGeometry
+		( Rocket::Core::CompiledGeometryHandle geometry, const Rocket::Core::Vector2f& translation )
 	{
 		RocketKGECompiledGeometry* p = (RocketKGECompiledGeometry*)geometry;
 		g_pRenderer->SetVertexDeclaration(m_pVD);
@@ -132,7 +140,8 @@ namespace kge
 	//------------------------------------------------------------------------------------
 	// Called by Rocket when it wants to release application-compiled geometry.
 	//------------------------------------------------------------------------------------
-	void libRocketKGERenderer::ReleaseCompiledGeometry( Rocket::Core::CompiledGeometryHandle geometry )
+	void libRocketKGERenderer::ReleaseCompiledGeometry
+		( Rocket::Core::CompiledGeometryHandle geometry )
 	{
 		RocketKGECompiledGeometry* p = (RocketKGECompiledGeometry*)geometry;
 		p->vertices->DecRef();
@@ -163,7 +172,8 @@ namespace kge
 	//------------------------------------------------------------------------------------
 	// Called by Rocket when a texture is required by the library.
 	//------------------------------------------------------------------------------------
-	bool libRocketKGERenderer::LoadTexture( Rocket::Core::TextureHandle& texture_handle, Rocket::Core::Vector2i& texture_dimensions, const Rocket::Core::String& source )
+	bool libRocketKGERenderer::LoadTexture( Rocket::Core::TextureHandle& texture_handle, 
+		Rocket::Core::Vector2i& texture_dimensions, const Rocket::Core::String& source )
 	{
 		gfx::Texture* p = g_pTextureManager->Load(source.CString(), NULL, NULL);
 		if (!p)
@@ -180,9 +190,22 @@ namespace kge
 	//------------------------------------------------------------------------------------
 	// Called by Rocket when a texture is required to be built from an internally-generated sequence of pixels.
 	//------------------------------------------------------------------------------------
-	bool libRocketKGERenderer::GenerateTexture( Rocket::Core::TextureHandle& texture_handle, const u8* source, const Rocket::Core::Vector2i& source_dimensions )
+	bool libRocketKGERenderer::GenerateTexture( Rocket::Core::TextureHandle& texture_handle, 
+		const u8* source, const Rocket::Core::Vector2i& source_dimensions )
 	{
-		return false;
+		// Create image resource
+		int datasize = source_dimensions.x * source_dimensions.y * 4;
+		gfx::Image* pimg = KGE_NEW(gfx::Image)(0, "libRocketTexture", (u8*)source, datasize, 
+			source_dimensions.x, source_dimensions.y, 0, 32, gfx::ETF_A8R8G8B8, 1);
+
+		// Create texture from an Image
+		gfx::Texture* pTex = g_pRenderer->CreateTexture(pimg);
+
+		KGE_DELETE(pimg, Image);
+
+		texture_handle = (Rocket::Core::TextureHandle)pTex;
+
+		return true;
 
 	} // GenerateTexture
 
@@ -214,5 +237,45 @@ namespace kge
 		return -0.5f;
 
 	} // GetVerticalTexelOffset
+
+	//------------------------------------------------------------------------------------
+	// Get the number of seconds elapsed since the start of the application.
+	//------------------------------------------------------------------------------------
+	float libRocketKGESystem::GetElapsedTime()
+	{
+		static core::Timer t;
+
+		return (float)t.GetTime(false) / 1000000.0f;
+
+	} // GetElapsedTime
+
+	//------------------------------------------------------------------------------------
+	// Log the specified message.
+	//------------------------------------------------------------------------------------
+	bool libRocketKGESystem::LogMessage( Rocket::Core::Log::Type type, const Rocket::Core::String& message )
+	{
+		switch (type)
+		{
+		case Rocket::Core::Log::Type::LT_ASSERT:
+		case Rocket::Core::Log::Type::LT_ERROR:
+			io::Logger::Error("LibRocket: %s", message.CString());
+			break;
+
+		case Rocket::Core::Log::Type::LT_DEBUG:
+			io::Logger::Debug("LibRocket: %s", message.CString());
+			break;
+
+		case Rocket::Core::Log::Type::LT_WARNING:
+			io::Logger::Warning("LibRocket: %s", message.CString());
+			break;
+
+		case Rocket::Core::Log::Type::LT_INFO:
+			io::Logger::Info("LibRocket: %s", message.CString());
+			break;
+		}
+		
+		return true;
+
+	} // LogMessage
 
 } // kge
